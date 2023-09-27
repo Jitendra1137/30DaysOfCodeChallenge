67f6baada1e070e08ff4fc8e187ceafe945fbbf0
Jitendra Kumar Jat <139442524+Jitendra1137@users.noreply.github.com>
2023-09-27 18:13:09 +0530

    Update element.js

--- day24/element.js
+++ day24/element.js
@@ -1,22 +1,22 @@
-function findPairsWithSum(arr, targetSum) {
-    const result = [];
-    const seen = new Set();
+function hasPairWithSum(arr, targetSum) {
+  const seen = new Set();
   
-    for (const num of arr) {
-      const complement = targetSum - num;
-      
-      if (seen.has(complement)) {
-        result.push([num, complement]);
-      }
-      
-      seen.add(num);
+  for (let num of arr) {
+    const complement = targetSum - num;
+    if (seen.has(complement)) {
+      return true;
     }
-  
-    return result;
+    seen.add(num);
   }
   
-  const arr = [1, 2, 3, 4, 5];
-  const targetSum = 10;
-  const pairs = findPairsWithSum(arr, targetSum);
-  console.log(pairs);
-  
+  return false;
+}
+
+const arr = [1, 2, 3, 4, 5];
+const targetSum = 10;
+
+if (hasPairWithSum(arr, targetSum)) {
+  console.log("There are elements that can make the sum:", targetSum);
+} else {
+  console.log("No elements can make the sum:", targetSum);
+}
